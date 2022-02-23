1448875f00d5bfea34679ac9763463d8bfbf8596
Nicole Candiotti <nccandiotti@gmail.com>
2022-02-22 19:09:31 -0500

    commit tree 42659a4d94035c432773fff82e3ff456a58bdc24

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    x = seconds
+    while x > 0 
+        puts "#{x} SECOND(S)!"
+        x -= 1
+   
+    end
+     "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    x = seconds
+    while x > 0 
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep 1
+    end
+     "HAPPY NEW YEAR!"
 end
